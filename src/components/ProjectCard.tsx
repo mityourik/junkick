60b87b6c5a33b7c7ff9b46f43050fcaca1d2a3cd
Dmitry <mityourik@gmail.com>
2025-08-11 23:49:34 +0300

    add styles to ProjectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const ProjectCard = () => {
   const [projectStatus, _setProjectStatus] = useState('В разработке');
   const [projectName, _setProjectName] = useState('Сайт доставки Bronfood');
   return (
-    <div className="card">
+    <Link to={`/projects/${projectName}`} className="card card--link">
       <div className="card__header">
         <p className="card__status">{projectStatus}</p>
       </div>
-      <h3 className="card__title">{projectName}</h3>
-      <p className="card__subtitle">Фронтенд на React TS, Redux Toolkit и Vite, бэк на Go</p>
-      {/* <div className="card__footer">
-        <button className="btn btn--primary">Подробнее</button>
-        <button className="btn btn--secondary">Участвовать</button>
-      </div> */}
-    </div>
+      <div className="card__body">
+        <h3 className="card__title">{projectName}</h3>
+        <p className="card__subtitle">Фронтенд на React TS, Redux Toolkit и Vite, бэк на Go</p>
+      </div>
+    </Link>
   );
 };
